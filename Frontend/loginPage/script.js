6309eff116fb5dff25b9c52077de59bcdb84e960
Sneha Sardesai <sneha.sardesai86@gmail.com>
2025-09-20 01:01:48 +0530

    Add Login Page and Create Account Page

---
+++ Frontend/loginPage/script.js
@@ -0,0 +1,8 @@
+function loginClicked()
+{
+    var username, password;
+    username = document.getElementById("nameInp").value;
+    password = document.getElementById("passInp").value;
+    console.log("Username : " + username + "\n" + " Password : " + password);
+}
+
